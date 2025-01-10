cb7ce1d4f18b4445be0709156f27827776c28f2c
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2025-01-10 20:58:58 -0300

    Criação da documentação da API via Swagger.

--- src/main/java/br/com/ecommerce/marcel/philippe/controller/CompraController.java
+++ src/main/java/br/com/ecommerce/marcel/philippe/controller/CompraController.java
@@ -19,6 +19,9 @@ import br.com.ecommerce.marcel.philippe.dto.CompraDTO;
 import br.com.ecommerce.marcel.philippe.dto.RelatorioDTO;
 import br.com.ecommerce.marcel.philippe.service.CompraService;
 import br.com.ecommerce.marcel.philippe.service.RelatorioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 
@@ -32,31 +35,51 @@ public class CompraController {
 	@Autowired
 	private RelatorioService relatorioService;
 
+	@Operation(summary = "Lista todas as compras")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de compras retornada com sucesso"), })
 	@GetMapping("/shopping")
+	@ResponseStatus(HttpStatus.OK)
 	public List<CompraDTO> getCompras() {
 		List<CompraDTO> produtos = compraService.getAll();
 		return produtos;
 	}
 
+	@Operation(summary = "Lista todas as compras de um usuário específico")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de compras retornada com sucesso"), })
 	@GetMapping("/shopping/shopByUser/{userIdentifier}")
+	@ResponseStatus(HttpStatus.OK)
 	public List<CompraDTO> getCompras(@PathVariable String userIdentifier) {
 		List<CompraDTO> produtos = compraService.getByUser(userIdentifier);
 		return produtos;
 	}
 
+	@Operation(summary = "Lista todas as compras de um usuário maior que uma determinada data")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de compras retornada com sucesso"), })
 	@GetMapping("/shopping/shopByDate")
-	public List<CompraDTO> getTodasAsCompras(@RequestParam String date) {
+	@ResponseStatus(HttpStatus.OK)
+	public List<CompraDTO> getTodasAsCompras(
+			@RequestParam(name = "data", required = true) @DateTimeFormat(pattern = "dd/MM/yyyy") String date) {
 		List<CompraDTO> produtos = compraService.getByDate(date);
 		return produtos;
 	}
-
+	
+	@Operation(summary = "Salvar Compras")
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Compras salvas com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Erro ao salvar compras") })
 	@PostMapping("/shopping")
 	@ResponseStatus(HttpStatus.CREATED)
 	public CompraDTO newCompra(@Valid @RequestHeader(name = "key", required=true) String key, @RequestBody CompraDTO compraDTO) {
 		return compraService.save(compraDTO, key);
 	}
 
+	@Operation(summary = "Lista todas as compras de um usuário a partir ou entre duas datas")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de compras retornada com sucesso"), })
 	@GetMapping("/shopping/search")
+	@ResponseStatus(HttpStatus.OK)
 	public List<CompraDTO> getComprasByFilter(
 			@RequestParam(name = "dataInicio", required = true) @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataInicio,
 			@RequestParam(name = "dataFim", required = false) @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataFim,
@@ -64,7 +87,11 @@ public class CompraController {
 		return relatorioService.getComprasByFilter(dataInicio, dataFim, valorMinimo);
 	}
 
+	@Operation(summary = "Lista todas as compras de um usuário entre duas datas")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de compras retornada com sucesso"), })
 	@GetMapping("/shopping/report")
+	@ResponseStatus(HttpStatus.OK)
 	public RelatorioDTO getRelatorioByDate(
 			@RequestParam(name = "dataInicio", required = true) @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataInicio,
 			@RequestParam(name = "dataFim", required = true) @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataFim) {
